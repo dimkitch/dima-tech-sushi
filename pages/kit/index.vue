a2055ad2e5b0a3a4ea6f535d5918a4f0b7e1d4b6
dimkitch <dimkitch@mail.ru>
2022-08-09 18:11:25 +0300

    remove block

--- pages/kit/index.vue
+++ pages/kit/index.vue
@@ -5,15 +5,6 @@
       <ContactsBlock label="тедефон" title="3241231512312"
         ><TelIcon
       /></ContactsBlock>
-
-      <!-- 
-    <div class="contacts-block">
-      <slot/>
-      <div class="contacts-block__info">
-        <p class="contacts-block__label">{{label}}</p>
-        <p class="contacts-block__title">{{title}}</p>
-      </div>
-    </div> -->
     </div>
   </main>
 </template>
